b7b873162c97dd8009f60744f310b2fd605424b7
devansh-mankar <Devanshmankar09@gmail.com>
2024-03-25 01:22:52 +0530

    create user model

---
+++ server/models/user.model.js
@@ -0,0 +1,27 @@
+import mongoose, { mongo } from "mongoose";
+import uniqueValidator from "mongoose-unique-validator";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
