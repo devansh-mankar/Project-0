cba5aa14425a5301cd4f1e8aacab303a0b8669c4
devansh-mankar <Devanshmankar09@gmail.com>
2024-03-25 01:09:25 +0530

    connect to db

--- server/index.js
+++ server/index.js
@@ -1,4 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
